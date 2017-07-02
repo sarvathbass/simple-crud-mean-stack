e59049d1954e72eaff9a9d0af68f5a87aac15597
sarvathbass <sarvath7@gmail.com>
2017-07-02 04:24:56 -0700

    done1.1

---
+++ README.md
@@ -0,0 +1,3 @@
+*run node server.js
+*configure config.json if needed else connected directly to mongodb server
+*tried asimple crud 
---
+++ app/app-content/app.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'body': {
+    'padding': [{ 'unit': 'px', 'value': 20 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 20 }, { 'unit': 'px', 'value': 0 }],
+    'backgroundColor': 'powderblue'
+  },
+  'flash-message': {
+    'marginTop': [{ 'unit': 'px', 'value': 20 }]
+  }
+});
